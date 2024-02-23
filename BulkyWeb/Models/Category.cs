c6b307732e709a5e389ab7730a2bda2a09e6015f
Odjoumani Tayo Déo-Gracias Giscar-Bonaventure <deogracias.odjoumani@gmail.com>
2024-02-23 11:52:56 -0500

    Ajout du context pour la tabe category et ajout d'un lien dans le html pour rejoindre la list de category

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key] //dit que Id est primary key
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
